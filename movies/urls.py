06a9fb48da7a0a27d5127d7e74b7184110873ecc
Machine <kuch.daw@gmail.com>
2021-10-17 18:30:24 +0200

    Adapted view names, changed new movie path

--- movies/urls.py
+++ movies/urls.py
@@ -4,12 +4,12 @@ from movies import views
 
 urlpatterns = [
     path("", views.movie_list, name="movie_list"),
-    path("view/<int:pk>", views.movie_view, name="movie_view"),
-    path("new", views.movie_create, name="movie_new"),
-    path("edit/<int:pk>", views.movie_update, name="movie_edit"),
+    path("view/<int:pk>", views.movie_details, name="movie_view"),
+    path("add", views.movie_add, name="movie_add"),
+    path("edit/<int:pk>", views.movie_edit, name="movie_edit"),
     path("delete/<int:pk>", views.movie_delete, name="movie_delete"),
-    path("search", views.get_movie, name="movie_search"),
-    path("filter", views.filter_movie, name="movie_filter"),
+    path("search", views.find_movie, name="movie_search"),
+    path("filter", views.filter_movies, name="movie_filter"),
     path("login", auth_views.LoginView.as_view(), name="login"),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
 ]
